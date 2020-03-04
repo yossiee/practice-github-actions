0e02d138f0985814149e74e6b381c1f571f41c66
yossiee <38056766+yossiee@users.noreply.github.com>
2020-03-04 14:19:24 +0900

    hoge

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,13 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+    public   function hofeusfa( )  {
+
+        $hoge = 'hog e'  ;
+
+$fuge = 'dsk';
+
+
+            return $hoge + $fuga  ;
+    }
 }
